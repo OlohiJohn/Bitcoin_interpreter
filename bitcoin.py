ee17e85491c391a291211ea8efd7d9dfac882b35
Olohi John <150522555+OlohiJohn@users.noreply.github.com>
2024-08-15 21:14:16 -0400

    Update bitcoin.py

--- bitcoin.py
+++ bitcoin.py
@@ -1,7 +1,9 @@
+#imports the relevant libraries
 import sys
 import requests
 import json
 
+#returns relevant errors if user enters a wrong argument
 if len(sys.argv) != 2:
     sys.exit("Missing command-line argument")
 
@@ -11,9 +13,9 @@ else:
     except:
         sys.exit("Command-line argument is not a number")
 
-response = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
-r = response.json()
+response = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json") #fetches up-to-date dollar-bitcoin exchange rate
+r = response.json() 
 #print(json.dumps(r, indent = 3))
 rate = r["bpi"]["USD"]["rate_float"]
-cost = rate * bitcoin
+cost = rate * bitcoin #conversion
 print(f"${cost:,.4f}")
